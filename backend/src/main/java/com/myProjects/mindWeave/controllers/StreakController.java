07d23da095bcea7e81e2502d753536fa1f5a04bd
HarikaK20 <harikakesharaju20@gmail.com>
2025-12-23 21:14:53 +0530

    Prepare backend for Render deployment

--- backend/src/main/java/com/myProjects/mindWeave/controllers/StreakController.java
+++ backend/src/main/java/com/myProjects/mindWeave/controllers/StreakController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,4 +28,12 @@ public class StreakController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+    
+    
+    @PostMapping("/daily-check")
+    public ResponseEntity<Void> runDailyStreakCheck() {
+        streakService.checkAndResetDailyStreaks();
+        return ResponseEntity.ok().build();
+    }
+
 }
